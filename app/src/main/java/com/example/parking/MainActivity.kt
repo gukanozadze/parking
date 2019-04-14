01c0a209bbec5fca1603bd05531d7e4edd333f82
Guka Nozadze <gukanozadze@gmail.com>
2019-04-14 22:43:06 +0400

    Widget working when reopened

--- app/src/main/java/com/example/parking/MainActivity.kt
+++ app/src/main/java/com/example/parking/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.parking
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,10 @@ import android.widget.RemoteViews
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.MetadataChanges
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+var number = ""
+var number_2 = ""
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "GUKADB"
@@ -22,48 +28,14 @@ class MainActivity : AppCompatActivity() {
         val location1 = parking.document("location1") // Reference to Document
         val location2 = parking.document("location2") // Reference to Document
 
-//        location1.get().addOnSuccessListener { document ->
-//            if (document != null) {
-//                val data = document.data
-//                val number = data?.get("number")
-//
-//                text_number.text = number.toString()
-//
-//                Log.d(TAG, "DocumentSnapshot data: $number")
-//
-//            } else {
-//                Log.d(TAG, "No such document")
-//            }
-//        }
-//        .addOnFailureListener { exception ->
-//            Log.d(TAG, "get failed with ", exception)
-//        }
-//
-//        location2.get().addOnSuccessListener { document ->
-//            if (document != null) {
-//                val data = document.data
-//                val number = data?.get("number")
-//
-//                text_number_2.text = number.toString()
-//
-//                Log.d(TAG, "DocumentSnapshot data: $number")
-//
-//            } else {
-//                Log.d(TAG, "No such document")
-//            }
-//        }.addOnFailureListener { exception ->
-//                Log.d(TAG, "get failed with ", exception)
-//        }
-
 
         // Listen for location 1.
         location1.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
             if (snapshot != null) {
                 val data = snapshot.data
                 if (data != null){
-                    val number = data["number"]
-                    text_number.text = number.toString()
-
+                    number = data["number"].toString()
+                    text_number.text = number
                 }
             }
         }
@@ -73,11 +45,21 @@ class MainActivity : AppCompatActivity() {
             if (snapshot != null) {
                 val data = snapshot.data
                 if (data != null){
-                    val number = data["number"]
-                    text_number_2.text = number.toString()
+                    number_2 = data["number"].toString()
+                    text_number_2.text = number_2
                 }
             }
         }
+
+
+        val intent = Intent(this, widgetText::class.java)
+        intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+        // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+        // since it seems the onUpdate() is only fired on that:
+        val ids = AppWidgetManager.getInstance(application)
+            .getAppWidgetIds(ComponentName(getApplication(), widgetText::class.java!!))
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+        sendBroadcast(intent)
 
     }
 }
--- app/src/main/java/com/example/parking/widgetText.kt
+++ app/src/main/java/com/example/parking/widgetText.kt
@@ -1,24 +1,47 @@
 package com.example.parking
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.CalendarView
 import android.widget.RemoteViews
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.MetadataChanges
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
-const val WIDGET_SYNC = "WIDGET_SYNC"
-
+var firebaseNumber = ""
 /**
  * Implementation of App Widget functionality.
  */
 class widgetText : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        Log.d("GUKADB", "UPDATED3")
+
+        // FIREBASE
+        val db = FirebaseFirestore.getInstance() // Firebase Refetence
+        val parking = db.collection("parking") // Collection
+        val location1 = parking.document("location1") // Reference to Document
+        location1.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
+            if (snapshot != null) {
+                val data = snapshot.data
+
+                if (data != null) firebaseNumber = data["number"].toString()
+
+                for (appWidgetId in appWidgetIds) {
+
+                    updateAppWidget(context, appWidgetManager, appWidgetId)
+                }
+            }
+        }
+
+
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
 
@@ -27,19 +50,11 @@ class widgetText : AppWidgetProvider() {
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
-    }
-    override fun onReceive(context: Context, intent: Intent?) {
-
-        if(WIDGET_SYNC == intent?.action){
-            val appWidgetId = intent.getIntExtra("appWidgetId", 0)
-            updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId)
-        }
-        super.onReceive(context, intent)
     }
 
     companion object {
@@ -48,31 +63,12 @@ class widgetText : AppWidgetProvider() {
             context: Context, appWidgetManager: AppWidgetManager,
             appWidgetId: Int
         ) {
-            var number = ""
-            val db = FirebaseFirestore.getInstance() // Firebase Refetence
-            val parking = db.collection("parking") // Collection
-            val location1 = parking.document("location1") // Reference to Document
-            location1.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
-                if (snapshot != null) {
-                    val data = snapshot.data
-                    if (data != null){
-                        number = data["number"].toString()
-                    }
-                }
-            }
 
-            val intent = Intent(context, widgetText::class.java)
-            intent.action = WIDGET_SYNC
-            intent.putExtra("AppWidgetID", appWidgetId)
-            val pendingIntent = PendingIntent.getBroadcast(context, 0,intent, 0)
-
-            // WIDGET MANAGER
-            val widgetText = number
-
+            Log.d("GUKADB", "UPDATED")
+            val widgetText = firebaseNumber
             // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.widget_text)
             views.setTextViewText(R.id.appwidget_text, widgetText)
-            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
